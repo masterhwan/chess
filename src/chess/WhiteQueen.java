47830c97a5aa2bcc3f3d27aa82cb41af24e7c727
slavehwan <slavehwan@gmail.com>
2018-05-21 11:39:13 +0900

    add Queen

---
+++ src/chess/WhiteQueen.java
@@ -0,0 +1,22 @@
+package chess;
+
+public class WhiteQueen {
+	private String color;
+	private String name;
+	private String display;
+	private double score;
+	private boolean move;
+
+	public WhiteQueen() {
+		color = "white";
+		name = "queen";
+		display = "q";
+		score = '4';
+		move = false;
+	}
+
+	public boolean isMove() {
+		return move;
+	}
+
+}
---
+++ test/chess/QueenTest.java
@@ -0,0 +1,19 @@
+package chess;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QueenTest {
+	@Test
+	public void moveBlackQueen() throws Exception {
+		BlackQueen bq = new BlackQueen();
+		assertFalse(bq.isMove());
+	}
+
+	@Test
+	public void moveWhiteQueen() throws Exception {
+		WhiteQueen wq = new WhiteQueen();
+		assertFalse(wq.isMove());
+	}
+}
